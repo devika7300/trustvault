ad08a477534f3ab7d251b0db9f0001046d67d54c
Devika <devikavid@gmail.com>
2024-10-22 16:05:59 -0400

    Implement the complete layout

---
+++ components/ui/DoughnutChart.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { Chart as ChartJS,ArcElement, Legend, Tooltip } from 'chart.js';
+import React from 'react'
+ChartJS.register(ArcElement, Tooltip,Legend)
+import { Doughnut } from 'react-chartjs-2';
+
+const DoughnutChart = ({accounts}:DoughnutChartProps) => {
+    const data = {
+        datasets:[
+            {
+                label: 'Banks',
+                data:[1250,4600,3460],
+                backgroundColor: ['#0747b6','#2265d8','#2f91fa']
+            }
+        ],
+        labels:['Bank 1','Bank 2','Bank 3']
+    }
+  return <Doughnut data={data} 
+  options={{
+    cutout:'60%',
+    plugins:{
+        legend:{
+            display:false
+        }
+    }
+  }}/>
+}
+
+export default DoughnutChart
